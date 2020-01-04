5a732e1aea493855330b698478a40621d1d68ad4
John Lewis <johnlewis@MacBook-Pro.local>
2020-01-04 14:47:39 -0700

    done 1-4

--- src/components/CurrencyDisplay.js
+++ src/components/CurrencyDisplay.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const CurrencyDisplay = (props) => (
 	<p>
-		US Dollar ${props.amount.toFixed(2)} - {props.currency.name}{' '}
-		{props.currency.symbol}
-		{(props.amount * props.currency.rate).toFixed(2)}
+		US Dollar ${props.amount.toFixed(2)} - {props.currencyData.name}{' '}
+		{props.currencyData.symbol}
+		{(props.amount * props.currencyData.rate).toFixed(2)}
 	</p>
 )
 
